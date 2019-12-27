42c6bbb63854ce614642342df84fd48575a2047b
ARiss Fire <55645042+arissfire@users.noreply.github.com>
2019-12-27 22:08:20 +0700

    Update mainsnake.py

--- mainsnake.py
+++ mainsnake.py
@@ -1,11 +1,28 @@
 import turtle
 import time
 import random
+import pygame
 
+
+#### Screen #####
 ar =turtle.Screen()
-ar.bgcolor("blue")
-ar.setup(width=900, height=900)
+ar.bgcolor("green")
+ar.setup(width=600, height=600)
 ar.tracer(0)
 ar.title("SNAKE GAME")
+
+
+ar.mainloop()
+
+
+score= turtle.Turtle()
+score.speed(0)
+score.shape("square")
+score.color("blue")
+score.penup()
+score.hideturtle()
+score.goto(0,260)
+score.write("Score: 0  High Score: 0", align="centter", font=("Courier", 28, "normal"))
+
 
 
